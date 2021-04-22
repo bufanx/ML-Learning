4485617d6ba4f0bc79a7a9a2fce627316276b188
bufanx <yczuishuai@outlook.com>
2021-04-22 17:41:58 +0800

    评价分类标准

--- PlayML/Metrics.py
+++ PlayML/Metrics.py
@@ -6,3 +6,55 @@ def accuracy_score(y_true, y_predict):
     assert y_true.shape[0] == y_predict.shape[0], \
         "the size of y_true and y_predict must equal"
     return sum(y_true == y_predict) / len(y_true)
+
+
+def TN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) & (y_predict == 0))
+
+
+def FP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 0) & (y_predict == 1))
+
+
+def FN(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) & (y_predict == 0))
+
+
+def TP(y_true, y_predict):
+    assert len(y_true) == len(y_predict)
+    return np.sum((y_true == 1) & (y_predict == 1))
+
+
+def confusion_matrix(y_true, y_predict):
+    return np.array([
+        [TN(y_true, y_predict), FP(y_true, y_predict)],
+        [FN(y_true, y_predict), TP(y_true, y_predict)]
+    ])
+
+
+def f1_score(precision, recall):
+    try:
+        return 2 * precision * recall / (precision + recall)
+    except:
+        return 0.
+
+
+def TPR(y_true, y_predict):
+    tp = TP(y_true, y_predict)
+    fn = FN(y_true, y_predict)
+    try:
+        return tp / (tp + fn)
+    except:
+        return 0.
+
+
+def FPR(y_true, y_predict):
+    fp = FP(y_true, y_predict)
+    tn = TN(y_true, y_predict)
+    try:
+        return fp / (fp + tn)
+    except:
+        return 0.
